c0d1d300cc69f3d4ce5707795b3c7d28b9f9aa8e
Piyush Sharma <piyushkumar40515s@gmail.com>
2026-08-27 15:07:23 +0530

    chore: ignore .open-next build output directory

--- .open-next/server-functions/default/.next/server/app/_global-error/page.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_global-error/page.js")
-R.c("server/chunks/ssr/node_modules__pnpm_1xjtgln._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__04zfdfo._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1fj3h01._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__05zjfav._.js")
-R.c("server/chunks/ssr/1nt__next_dist_client_components_builtin_global-error_0rjzhot.js")
-R.c("server/chunks/ssr/_next-internal_server_app__global-error_page_actions_0zi5s8-.js")
-R.m(31422)
-module.exports=R.m(31422).exports
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LtKpK/7guuk70LyabXeO/oyzJUnRNiVLH0qzE2Zh0F4=\"\n}"
